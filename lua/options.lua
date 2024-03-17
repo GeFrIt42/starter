cab488ecd112c02fcf70d3fff3f77954b433e2fa
gefrit <gefrit42@gmail.com>
2024-03-17 09:13:39 -0400

    common: migrate to NvChadv2.5, set theme, set vim options

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,11 @@
 require "nvchad.options"
 
 -- add yours here!
+local opt = vim.opt
+local o = vim.o
+local g = vim.g
+
+o.mouse = ""
+opt.list = true -- shot traling characters
+vim.api.nvim_set_option_value("colorcolumn", "80", {})
+opt.relativenumber = true
