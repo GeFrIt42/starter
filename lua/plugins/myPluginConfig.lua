d5896f7d22885debe4e57ea8e0fac7aef8e8015c
gefrit <gefrit42@gmail.com>
2024-03-17 10:59:18 -0400

    common: NvChad cus config, migrate activation Mason install needed LSP

--- lua/plugins/myPluginConfig.lua
+++ lua/plugins/myPluginConfig.lua
@@ -1,6 +1,5 @@
 local plugins = {
-
-  -- this opts will extend the default opts 
+  -- inteligent code highlight
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -19,6 +18,30 @@ local plugins = {
         "python",
         "ruby",
         "vim",
+      },
+    },
+  },
+
+  -- mason installs executables needed by lspconfig
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- lua stuff
+        "lua-language-server",
+        "stylua",
+
+        -- web dev stuff
+        -- "html-lsp",
+        "prettier",
+        "marksman",
+        "bash-language-server"
+        -- c/cpp stuff
+        -- clangd and clang-format need dynamic linking that in nix works different
+        -- install them in nix way to avoid dinamic linking clash
+        -- https://github.com/Mic92/nix-ld
+        --"clangd",
+        --"clang-format",
       },
     },
   },
