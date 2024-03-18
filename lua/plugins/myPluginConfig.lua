3944a1a2b74affa5a2eb8a2ca056a8396802b7c1
gefrit <gefrit42@gmail.com>
2024-03-17 20:04:31 -0400

    common: NvChad cus config, migrate lspconfig

--- lua/plugins/myPluginConfig.lua
+++ lua/plugins/myPluginConfig.lua
@@ -45,6 +45,15 @@ local plugins = {
       },
     },
   },
+
+  -- In order to modify the `lspconfig` configuration:
+  {
+  "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+   end,
+  },
 }
 
 return plugins
