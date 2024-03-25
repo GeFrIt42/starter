c6f3d5cb288335939cbf3f2e8947c9cab390c5bf
gefrit <gefrit42@gmail.com>
2024-03-25 00:29:40 -0400

    common: vimspector debug key bindings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -12,6 +12,7 @@ end, { desc = "File Format with conform" })
 
 map("i", "jk", "<ESC>", { desc = "Escape insert mode" })
 
+-- Vimspector keys mapping
 -- " mnemonic 'di' = 'debug inspect' (pick your own, if you prefer!)
 -- " for normal mode - the word under the cursor
 -- nmap <Leader>di <Plug>VimspectorBalloonEval
@@ -27,26 +28,26 @@ map("n", "ff", "<Plug>VimspectorBalloonEval", { desc = "VimspectorBalloonEval in
 
 -- debug mapping
 -- F5
--- map("n", "ff", "<Plug>VimspectorContinue", { desc = "When debugging, continue. Otherwise start debugging." })
+map("n", "<F7>", "<Plug>VimspectorContinue", { desc = "When debugging, continue. Otherwise start debugging." })
 -- F3
--- map("n", "ff", "<Plug>VimspectorStop", { desc = "Stop debugging." })
+map("n", "<F8>", "<Plug>VimspectorStop", { desc = "Stop debugging." })
 -- F4
-map("n", "<F2>", "<Plug>VimspectorRestart", { desc = "Restart debugging with the same configuration." })
+map("n", "<F9>", "<Plug>VimspectorRestart", { desc = "Restart debugging with the same configuration." })
 -- F6
 -- map("n", "ff", "<Plug>VimspectorPause", { desc = "Pause debuggee." })
 -- F9
-map("n", "<F3>", "<Plug>VimspectorToggleBreakpoint", { desc = "Toggle line breakpoint on the current line." })
+map("n", "<F5>", "<Plug>VimspectorToggleBreakpoint", { desc = "Toggle line breakpoint on the current line." })
 -- <leader>F9
--- map("n", "ff", "<Plug>VimspectorToggleConditionalBreakpoint", { desc = "Toggle conditional line breakpoint or logpoint on the current line." })
+-- map("n", "<F7>", "<Plug>VimspectorToggleConditionalBreakpoint", { desc = "Toggle conditional line breakpoint or logpoint on the current line." })
 -- F8
--- map("n", "ff", "<Plug>VimspectorAddFunctionBreakpoint", { desc = "Add a function breakpoint for the expression under cursor" })
+map("n", "<F6>", "<Plug>VimspectorAddFunctionBreakpoint", { desc = "Add a function breakpoint for the expression under cursor" })
 -- <leader>F8
 -- map("n", "ff", "<Plug>VimspectorRunToCursor", { desc = "Run to Cursor" })
 -- F10
-map("n", "F4", "<Plug>VimspectorStepOver", { desc = "Step Over" })
+map("n", "<F2>", "<Plug>VimspectorStepOver", { desc = "Step Over" })
 -- F11
--- map("n", "ff", "<Plug>VimspectorStepInto", { desc = "Step Into" })
+map("n", "<F3>", "<Plug>VimspectorStepInto", { desc = "Step Into" })
 -- F12
--- map("n", "ff", "<Plug>VimspectorStepOut", { desc = "Step out of current function scope" })
+map("n", "<F4>", "<Plug>VimspectorStepOut", { desc = "Step out of current function scope" })
 --
 --
